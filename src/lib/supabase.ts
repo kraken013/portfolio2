15da62667cbc221f0f6c4b9aae8bfc61aaf2d9ae
kraken013 <sofiane.flici14@gmail.com>
2024-12-03 21:57:36 +0100

    create env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://cindeyvlqtoutxffhwmj.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpbmRleXZscXRvdXR4ZmZod21qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMxODQ0NDksImV4cCI6MjA0ODc2MDQ0OX0.W4CUUJtrH1zfhQgRvcvM0wy2z26zGnE08l_zxJ1gHUQ";
+const supabaseUrl = import.meta.env.VITE_SUPA_URL;
+const supabaseKey = import.meta.env.VITE_SUPA_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
